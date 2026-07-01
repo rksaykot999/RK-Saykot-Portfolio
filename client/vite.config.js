e8f3dc92e908e593e4aec833e643ba7a2a13dd20
RK Saykot <rksaikatkhan999@gmail.com>
2026-07-01 12:40:57 +0600

    site deployed to gihub and vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Saykot/',   // Required for GitHub Pages deployment
+  base: process.env.VERCEL ? '/' : '/Saykot/', 
   server: {
     proxy: {
-      // Forwards /api/* requests to Express during development
       '/api': 'http://localhost:5000'
     }
   }
 })
-
