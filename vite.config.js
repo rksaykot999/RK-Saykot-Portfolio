52ecc3d5fc2d734d634182f91959100b0087ba6f
RK Saykot <rksaikatkhan999@gmail.com>
2026-07-01 18:39:00 +0600

    problem solved

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/RK-Saykot-Portfolio/',
+  base: process.env.VITE_BASE || '/',
   plugins: [react()],
 })
